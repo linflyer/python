67fd706f02202b2788a4d33c8d4541993575dc62
dimdym <dimdym@gmail.com>
2014-09-03 11:48:16 -0500

    added method to print batches containing the same patients

--- i2b2/merge-vectors.py
+++ i2b2/merge-vectors.py
@@ -19,12 +19,34 @@ def patients_in_multiple_files():
         elements = line.strip().split(',')
         patient_num = elements[0]
         if(patient2vector.has_key(patient_num)):
-          print file
+          print patient_num
+          # print file
           # print patient2vector[patient_num]
           # print line.strip()
           # print
         else:
           patient2vector[patient_num] = line.strip()
+
+def batches_with_same_patients():
+  """ """
+
+  patient2batch = {}
+  for file in glob.glob(patient_files):
+    for line in open(file):
+      if line[0].isdigit():
+        elements = line.strip().split(',')
+        patient_num = elements[0]
+        if(patient2batch.has_key(patient_num)):
+          patient2batch[patient_num].append(file)
+        else:
+          patient2batch[patient_num] = [file]
+
+  batches_containing_same_patients = set()
+  for key in patient2batch.keys():
+    if len(patient2batch[key]) > 1:
+      batches_containing_same_patients.update(patient2batch[key])
+
+  print batches_containing_same_patients
 
 def sum(vector1, vector2):
   """Sum two vectors represented as lists. Element 0 is patient_num."""
@@ -41,4 +63,4 @@ def sum(vector1, vector2):
 
 if __name__ == "__main__":
 
-  patients_in_multiple_files()
+  batches_with_same_patients()
