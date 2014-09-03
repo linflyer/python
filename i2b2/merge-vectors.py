ca87a80e5b3b9cf013827a9d462a85544b732904
dimdym <dimdym@gmail.com>
2014-09-03 14:49:57 -0500

    added method to remove duplicate patients from labels

--- i2b2/merge-vectors.py
+++ i2b2/merge-vectors.py
@@ -8,6 +8,7 @@ Make a single vector for those patients but summing the individual ones.
 import glob
 
 patient_files = "/Users/dima/Boston/Data/Phenotype/T2D/Pei/trial3/*_pt.txt"
+labels = "/Users/dima/Boston/Data/Phenotype/T2D/Data/labels-original.txt"
 
 def patients_in_multiple_files():
   """List of patient num(s) that have vectors in multiple files"""
@@ -19,16 +20,15 @@ def patients_in_multiple_files():
         elements = line.strip().split(',')
         patient_num = elements[0]
         if(patient2vector.has_key(patient_num)):
-          print patient_num
-          # print file
-          # print patient2vector[patient_num]
-          # print line.strip()
-          # print
+          print file
+          print patient2vector[patient_num]
+          print line.strip()
+          print
         else:
           patient2vector[patient_num] = line.strip()
 
 def batches_with_same_patients():
-  """ """
+  """Find batches (*_pt.txt files) that have repeating patient vectors"""
 
   patient2batch = {}
   for file in glob.glob(patient_files):
@@ -48,6 +48,25 @@ def batches_with_same_patients():
 
   print batches_containing_same_patients
 
+def remove_repeating_patients_from_labels():
+  """Some patients are labeled twice. Remove them."""
+
+  # find duplicate patient_num(s)
+  patients = set()
+  double_annotated_patients = set()
+  for line in open(labels):
+    patient_num, label = line.strip().split(',')
+    if patient_num in patients:
+      double_annotated_patients.add(patient_num)
+    else:
+      patients.add(patient_num)
+
+  # print non-duplicate patient_num(s) and labels
+  for line in open(labels):
+    patient_num, label = line.strip().split(',')
+    if not patient_num in double_annotated_patients:
+      print line.strip()
+
 def sum(vector1, vector2):
   """Sum two vectors represented as lists. Element 0 is patient_num."""
 
@@ -63,4 +82,4 @@ def sum(vector1, vector2):
 
 if __name__ == "__main__":
 
-  batches_with_same_patients()
+  remove_repeating_patients_from_labels()
