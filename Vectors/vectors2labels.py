cbedf516d4234ab22534703f40ec22341e5b1d26
dimdym <dimdym@gmail.com>
2015-07-27 14:10:54 -0500

    finished udpates; should be working now

--- Vectors/vectors2labels.py
+++ Vectors/vectors2labels.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -B                                                                      
+#!/usr/bin/python -B         
 
 """
 Map word2vec vectors to labels stored in a '|' separated file.
@@ -10,23 +10,24 @@ NEGEXAMPLES = 2000
 DIMENSIONS = 200
 CUI2TUI = '/Users/dima/Boston/DictLookup/cui2tui.txt'
 TUI2SEMTYPE = '/Users/dima/Boston/DictLookup/tui2semtype.txt'
-VECTORS = '/Users/dima/Boston/Vectors/SemType/pmc-w2v-200k.txt'
+VECTORS = '/Users/dima/Boston/Vectors/SemType/100K/pmc-w2v-100k.txt'
+# VECTORS = '/Users/dima/Boston/Vectors/Models/PubMed-w2v.txt'
 
 def read_cui_tui_map():
   """ """
   
   # key: word, value: set of tuis
-  tui_lookup = {}
+  cui2tui = {}
 
   for line in open(CUI2TUI):
     if line.startswith('CUI'):
       continue # header
     cui, word, tui = line.strip().split('|')
-    if not tui_lookup.has_key(word):
-      tui_lookup[word] = set()
-    tui_lookup[word].add(tui)
+    if not cui2tui.has_key(word):
+      cui2tui[word] = set()
+    cui2tui[word].add(tui)
 
-  return tui_lookup
+  return cui2tui
 
 def read_tui_semtype_map():
   """ """
@@ -42,7 +43,25 @@ def read_tui_semtype_map():
 
   return tui2semtype
 
-def libsvm_vector(word2vec_vector):
+def get_cui_semtype_map():
+  """ """
+
+  cui2tui = read_cui_tui_map()
+  tui2semtype = read_tui_semtype_map()
+
+  # key: word, value: set ofsemantic types
+  cui2semtype = {}
+
+  for cui, tuis in cui2tui.items():
+    semtypes = set()
+    for tui in tuis:
+      semtype = tui2semtype[tui]
+      semtypes.add(semtype)
+    cui2semtype[cui] = semtypes
+
+  return cui2semtype
+
+def make_libsvm_vector(word2vec_vector):
   """Convert a list of numbers to a libsvm vector"""
   
   vector = []
@@ -55,17 +74,20 @@ def libsvm_vector(word2vec_vector):
 if __name__ == "__main__":
   """Form a training set by mapping vectors to word semantic types"""
 
-  label_lookup = read_labels()
-  
+  cui2semtype = get_cui_semtype_map()
+
+  print cui2semtype['ms']
+  exit()
+ 
   for line in open(VECTORS):
     elements = line.split()
     word = elements[0]
-    if label_lookup.has_key(word):
-      label_set = label_lookup[word]
+    if cui2semtype.has_key(word):
+      label_set = cui2semtype[word]
       if len(label_set) > 1:
         continue # skip polysemous words
-      vector = libsvm_vector(elements[1:DIMENSIONS + 1])
-      label = TOINT[list(label_set)[0]]
+      vector = make_libsvm_vector(elements[1:DIMENSIONS + 1])
+      label = list(label_set)[0]
       print label, vector
 
   # sample N words with no semantic type
@@ -73,13 +95,11 @@ if __name__ == "__main__":
   for line in open(VECTORS):
     elements = line.split()
     word = elements[0]
-
     if len(elements) < DIMENSIONS:
       continue # skip word2vec model header
     if sampled_so_far >= NEGEXAMPLES:
       break
-
-    if not label_lookup.has_key(word):
+    if not cui2semtype.has_key(word):
       sampled_so_far = sampled_so_far + 1
       vector = libsvm_vector(elements[1:DIMENSIONS + 1])
       print NEGLABEL, vector
